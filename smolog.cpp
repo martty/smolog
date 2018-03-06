4e5235342c930bd27e1a90164ca8721edf3ae648
Marcell Kiss <marcellk.kiss@gmail.com>
2018-03-06 21:51:53 +0100

    Initial commit

---
+++ smolog.cpp
@@ -0,0 +1,258 @@
+//
+// Copyright(c) 2018 Marcell Kiss
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+//
+
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <utility>
+#include <vector>
+#include <mutex>
+
+#include "smolog.hpp"
+
+namespace smolog {
+	struct _logger_state {
+		std::vector<std::shared_ptr<sink>> sinks;
+
+		// internal buffer to format to
+		std::vector<char> buf;
+		std::string name;
+
+		_logger_state(std::string name) : name(std::move(name)) {}
+	};
+
+	logger::~logger() {}
+
+	constexpr const char * logger::level_as_string(Level level) const {
+		switch (level) {
+		case Level::Off: return "off";
+		case Level::Trace: return "trace";
+		case Level::Debug: return "debug";
+		case Level::Info: return "info";
+		case Level::Warning: return "warn";
+		case Level::Error: return "error";
+		case Level::Critical: return "critical";
+		default: return "??";
+		}
+	}
+
+	void logger::add_sink(std::shared_ptr<sink> s) {
+		_internal->sinks.emplace_back(std::move(s));
+	}
+
+	void logger::remove_sink(sink * to_remove) {
+		for (auto it = _internal->sinks.begin(); it != _internal->sinks.end(); it++) {
+			if (it->get() == to_remove) {
+				_internal->sinks.erase(it);
+				return;
+			}
+		}
+	}
+
+	void logger::remove_sink(std::shared_ptr<sink>& s) {
+		remove_sink(s.get());
+	}
+
+	// [ date time ] [level] [logger_name] message
+	void logger::emit_prompt(Level level) {
+		auto& buf = _internal->buf;
+		auto& name = _internal->name;
+		std::time_t t = std::time(nullptr);
+		char time_buf[100];
+		std::strftime(time_buf, sizeof time_buf, "[ %D %T ]", std::gmtime(&t));
+		auto time_size = strlen(time_buf);
+		auto buf_size = buf.size();
+		auto lnsize = snprintf(nullptr, 0, " [%s] [%s] ", level_as_string(level), name.c_str());
+		buf.resize(buf_size + time_size + lnsize + 1);
+		strcpy_s(buf.data() + buf_size, time_size + 1, time_buf);
+		snprintf(buf.data() + buf_size + time_size, lnsize + 1, " [%s] [%s] ", level_as_string(level), name.c_str());
+		// remove trailing \0
+		buf.resize(buf.size() - 1);
+	}
+	void logger::format(const char * fmt, va_list args1) {
+		auto& buf = _internal->buf;
+		va_list args2;
+		va_copy(args2, args1);
+
+		auto size_req = 1 + std::vsnprintf(NULL, 0, fmt, args1);
+		va_end(args1);
+
+		auto prev_size = buf.size();
+		buf.resize(buf.size() + size_req + 1);
+
+		std::vsnprintf(buf.data() + prev_size, size_req, fmt, args2);
+		va_end(args2);
+
+		// write newline
+		buf[buf.size() - 2] = '\n';
+		buf[buf.size() - 1] = '\0';
+	}
+	void logger::_log(Level level, const char * fmt, ...) {
+		auto& buf = _internal->buf;
+		auto& sinks = _internal->sinks;
+
+		if (current_level > level) return;
+		emit_prompt(level);
+		va_list args;
+		va_start(args, fmt);
+		format(fmt, args);
+		va_end(args);
+
+		for (auto& sink : sinks) {
+			sink->write({ buf.data(), buf.size() - 1 /* like strlen */, level });
+		}
+
+		if (level >= flush_level)
+			flush();
+
+		buf.clear();
+	}
+	void logger::flush() {
+		auto& sinks = _internal->sinks;
+		for (auto& sink : sinks) {
+			sink->flush();
+		}
+	}
+	logger::logger(const char * name) : _internal(std::make_unique<_logger_state>(name)){}
+
+	void stdout_sink::write(const message & msg) {
+		fputs(msg.str, stdout);
+	}
+	void stdout_sink::flush() {
+		::fflush(stdout);
+	}
+	void stderr_sink::write(const message & msg) {
+		fputs(msg.str, stdout);
+	}
+	void stderr_sink::flush() {
+		::fflush(stdout);
+	}
+};
+
+// Adapted from spdlog https://github.com/gabime/spdlog/blob/master/include/spdlog/sinks/wincolor_sink.h
+// Copyright(c) 2016 spdlog
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+//
+
+#ifdef _WIN32 
+#include <Windows.h>
+#include <Wincon.h>
+namespace smolog {
+	static const WORD BOLD = FOREGROUND_INTENSITY;
+	static const WORD RED = FOREGROUND_RED;
+	static const WORD CYAN = FOREGROUND_GREEN | FOREGROUND_BLUE;
+	static const WORD WHITE = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+	static const WORD YELLOW = FOREGROUND_RED | FOREGROUND_GREEN;
+
+	struct _wincolor_state {
+		HANDLE out_handle;
+		std::vector<WORD> colors;
+
+		_wincolor_state(HANDLE out_handle) : out_handle(out_handle) {}
+	};
+
+	wincolor_sink::~wincolor_sink() {}
+
+	wincolor_sink::wincolor_sink(HANDLE std_handle) : _internal(std::make_unique<_wincolor_state>(std_handle)){
+		auto& colors = _internal->colors;
+		colors.resize(smolog::Level::Critical + 1);
+		colors[smolog::Level::Trace] = CYAN;
+		colors[smolog::Level::Debug] = CYAN;
+		colors[smolog::Level::Info] = WHITE | BOLD;
+		colors[smolog::Level::Warning] = YELLOW | BOLD;
+		colors[smolog::Level::Error] = RED | BOLD; // red bold
+		colors[smolog::Level::Critical] = BACKGROUND_RED | WHITE | BOLD; // white bold on red background
+		colors[smolog::Level::Off] = 0;
+	}
+
+	void wincolor_sink::set_color(smolog::Level level, WORD color) {
+		_internal->colors[level] = color;
+	}
+
+	void wincolor_sink::write(const message & msg) {
+		auto color = _internal->colors[msg.level];
+		auto orig_attribs = set_console_attribs(color);
+		::WriteConsoleA(_internal->out_handle, msg.str, static_cast<DWORD>(msg.size), nullptr, nullptr);
+		::SetConsoleTextAttribute(_internal->out_handle, orig_attribs); //reset to orig colors
+	}
+
+	// set color and return the orig console attributes (for resetting later)
+	WORD wincolor_sink::set_console_attribs(WORD attribs){
+		CONSOLE_SCREEN_BUFFER_INFO orig_buffer_info;
+		::GetConsoleScreenBufferInfo(_internal->out_handle, &orig_buffer_info);
+		WORD back_color = orig_buffer_info.wAttributes;
+		// retrieve the current background color
+		back_color &= static_cast<WORD>(~(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY));
+		// keep the background color unchanged
+		::SetConsoleTextAttribute(_internal->out_handle, attribs | back_color);
+		return orig_buffer_info.wAttributes; //return orig attribs
+	}
+
+	wincolor_stdout_sink::wincolor_stdout_sink() : wincolor_sink(GetStdHandle(STD_OUTPUT_HANDLE)) {}
+
+	wincolor_stderr_sink::wincolor_stderr_sink() : wincolor_sink(GetStdHandle(STD_ERROR_HANDLE)) {}
+#endif
+#ifdef _MSC_VER
+	void msvc_sink::write(const message & msg) {
+		OutputDebugStringA(msg.str);
+	}
+};
+#endif
+
+namespace smolog {
+	struct _dist_state {
+		std::vector<std::shared_ptr<sink>> sinks;
+	};
+
+	void dist_sink::write(const message & msg) {
+		for (auto& sink : _internal->sinks) {
+			sink->write(msg);
+		}
+	}
+
+	dist_sink::dist_sink() : _internal(std::make_unique<_dist_state>()){}
+
+	void dist_sink::add_sink(std::shared_ptr<sink> s) {
+		_internal->sinks.emplace_back(std::move(s));
+	}
+
+	void dist_sink::remove_sink(sink * to_remove) {
+		for (auto it = _internal->sinks.begin(); it != _internal->sinks.end(); it++) {
+			if (it->get() == to_remove) {
+				_internal->sinks.erase(it);
+				return;
+			}
+		}
+	}
+
+	struct _file_state {
+		FILE * fd;
+	};
+
+	file_sink::file_sink(const char * filename, bool truncate) : _internal(std::make_unique<_file_state>()){
+		const char * open_flags = truncate ? "w" : "a";
+		int err = fopen_s(&_internal->fd, filename, open_flags);
+	}
+
+	void file_sink::write(const message & msg) {
+		fputs(msg.str, _internal->fd);
+	}
+
+	void file_sink::flush() {
+		fflush(_internal->fd);
+	}
+
+	file_sink::~file_sink() {
+		fclose(_internal->fd);
+	}
+
+	mt_sink::mt_sink(std::shared_ptr<sink> l) : wrapped(std::move(l)) {}
+
+	void mt_sink::write(const message & msg) {
+		static std::mutex mut;
+		std::lock_guard<std::mutex> _(mut);
+		wrapped->write(msg);
+	}
+};
---
+++ smolog.hpp
@@ -0,0 +1,152 @@
+//
+// Copyright(c) 2018 Marcell Kiss
+// Distributed under the MIT License (http://opensource.org/licenses/MIT)
+//
+
+#pragma once
+
+#include <cstdarg>
+#include <memory>
+
+namespace smolog {
+	enum Level : unsigned char {
+		Off, Trace, Debug, Info, Warning, Error, Critical
+	};
+
+	// sinks receive this struct to write
+	struct message {
+		const char * str;
+		size_t size;
+		Level level;
+	};
+
+	// base class for sinks
+	struct sink {
+		virtual void write(const message& msg) = 0;
+		virtual void flush() {};
+		virtual ~sink() {}
+	};
+
+	struct logger {
+		// all messages below this level will be discarded
+		Level current_level = Level::Debug;
+		// all messages received at this or higher level will be flushed
+		Level flush_level = Level::Debug;
+
+		void add_sink(std::shared_ptr<sink>);
+		void remove_sink(sink*);
+		void remove_sink(std::shared_ptr<sink>&);
+	
+		void _log(Level level, const char* fmt, ...);
+		void flush();
+
+		logger(const char* name);
+		~logger();
+	private:
+		// [ date time ] [level] [logger_name] message
+		void emit_prompt(Level level);
+		void format(const char* fmt, va_list args1);
+		constexpr const char* level_as_string(Level level) const;
+
+		std::unique_ptr<struct _logger_state> _internal;
+	};
+};
+
+// While very ugly, these macros enable cross-compiler checking of the format strings
+// These can be removed as MSVC implements an attribute to check user-defined functions
+// We rely on the compiler frontend not figuring out that sqrtf(4.0f) is always "true"
+
+#ifndef SMOLOG_DISABLE_MACROS
+#define trace(...) _log(smolog::Level::Trace, __VA_ARGS__), sqrtf(4.0f) || printf(__VA_ARGS__);
+#define debug(...) _log(smolog::Level::Debug, __VA_ARGS__), sqrtf(4.0f) || printf(__VA_ARGS__);
+#define info(...) _log(smolog::Level::Info, __VA_ARGS__), sqrtf(4.0f) || printf(__VA_ARGS__);
+#define warn(...) _log(smolog::Level::Warning, __VA_ARGS__), sqrtf(4.0f) || printf(__VA_ARGS__);
+#define error(...) _log(smolog::Level::Error, __VA_ARGS__), sqrtf(4.0f) || printf(__VA_ARGS__);
+#define critical(...) _log(smolog::Level::Critical, __VA_ARGS__), sqrtf(4.0f) || printf(__VA_ARGS__);
+#define log_at(level, ...) _log(level, __VA_ARGS__), sqrtf(4.0f) || printf(__VA_ARGS__);
+#endif
+
+/****************
+*
+*    SINKS
+*
+****************/
+
+namespace smolog {
+	struct stdout_sink : sink {
+		virtual void write(const message& msg) override;
+		virtual void flush() override;
+	};
+	
+	struct stderr_sink : sink {
+		virtual void write(const message& msg) override;
+		virtual void flush() override;
+	};
+
+#ifdef _WIN32
+#ifndef HANDLE
+	using HANDLE = void*;
+#endif
+#ifndef WORD
+	using WORD = unsigned short;
+#endif
+	struct wincolor_sink : public sink {
+
+		wincolor_sink(HANDLE std_handle);
+		~wincolor_sink();
+
+		// change the color for the given level
+		void set_color(smolog::Level level, WORD color);
+
+		void write(const message& msg) override;
+
+	private:
+		WORD set_console_attribs(WORD attribs);
+
+		std::unique_ptr<struct _wincolor_state> _internal;
+	};
+
+	struct wincolor_stdout_sink : public wincolor_sink {
+		wincolor_stdout_sink();
+	};
+
+	struct wincolor_stderr_sink : public wincolor_sink {
+		wincolor_stderr_sink();
+	};
+#endif
+#ifdef _MSC_VER
+	struct msvc_sink : sink {
+		virtual void write(const message& msg) override;
+	};
+#endif
+	struct dist_sink : public sink {
+		dist_sink();
+
+		void add_sink(std::shared_ptr<sink>);
+		void remove_sink(sink*);
+
+		virtual void write(const message& msg) override;
+	private:
+		std::unique_ptr<struct _dist_state> _internal;
+	};
+
+	struct file_sink : public sink {
+		file_sink(const char * filename, bool truncate = false);
+
+		virtual void write(const message& msg) override;
+
+		virtual void flush() override;
+
+		~file_sink();
+	private:
+		std::unique_ptr<struct _file_state> _internal;
+	};
+
+	struct mt_sink : public sink {
+		mt_sink(std::shared_ptr<sink> l);
+
+		virtual void write(const message& msg) override;
+	private:
+		std::shared_ptr<sink> wrapped;
+	};
+};
